403240d5c7daf310ccb7a9823e16a95f681a31e3
fauzandwip <fauzan27dp@gmail.com>
2024-02-08 06:51:30 +0700

    setup project install needed packages setup server node

---
+++ app.js
@@ -0,0 +1,15 @@
+if (process.env.NODE !== 'production') {
+	require('dotenv').config();
+}
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(require('./routes'));
+
+module.exports = app;
