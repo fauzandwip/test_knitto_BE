0a2335bafed45721981a34b9d15d0792fd128716
fauzandwip <fauzan27dp@gmail.com>
2024-02-09 06:51:47 +0700

    feat: create routes and controller to update product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -30,6 +30,37 @@ class ProductController {
 			next(error);
 		}
 	}
+
+	static async updateProduct(req, res, next) {
+		try {
+			const { id } = req.params;
+			const { title, description, price, stock, brand, category, thumbnail } =
+				req.body;
+
+			const product = await Product.findByPk(id);
+
+			if (!product) {
+				throw {
+					name: 'NotFound',
+					message: 'Product not found',
+				};
+			}
+
+			product.update({
+				title,
+				description,
+				price,
+				stock,
+				brand,
+				category,
+				thumbnail,
+			});
+
+			res.status(200).json({ message: `Product sucsess to update` });
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
 module.exports = ProductController;
--- routes/products/index.js
+++ routes/products/index.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 
 router.get('/', ProductController.getProducts);
 router.post('/', ProductController.addProduct);
+router.put('/:id', ProductController.updateProduct);
 
 module.exports = router;
